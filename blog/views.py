15c23e0b00ca138dc65056b04552ec0a747feded
Mauricio Roseo <mauriciornn@gmail.com>
2023-06-21 11:01:14 -0300

    Initial commit.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Olá Django - index')
+    return render(request, 'index.html')
 
 def ola(request):
     return HttpResponse('Olá django')
