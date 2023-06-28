ba47147eaf2e2da0801d8bdda20e9d76be217d31
Mauricio Roseo <mauriciornn@gmail.com>
2023-06-28 11:09:35 -0300

    Introdução aos templates Django

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
 def ola(request):
     return HttpResponse('Olá django')
