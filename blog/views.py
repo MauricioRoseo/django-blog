5d6c1a82b6fce36202e91d788c8b8d169d304aba
Mauricio <mauriciornn@gmail.com>
2023-08-16 10:36:48 -0300

    Inclui o model Post.

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from blog.models import Post
+
 def index(request):
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
-def ola(request):
-    return HttpResponse('Olá django')
+def ola(request): # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
