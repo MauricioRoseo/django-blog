71d62661cc070423b10ca0fa030e6a5d142c946c
Mauricio <mauriciornn@gmail.com>
2023-10-25 10:20:42 -0300

    UPDATE - CRUD Básico.

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.views.generic import DetailView, ListView, TemplateView
 
 import json
 from django.core.serializers.json import DjangoJSONEncoder
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
@@ -74,6 +74,11 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
         return super(PostCreateView, self).form_valid(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostCreateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Criando um post'
+        return context
 
 
 @csrf_exempt
@@ -105,3 +110,19 @@ class PostListView(ListView):
 
 class SobreTemplateView(TemplateView):
     template_name = 'post/sobre.html'
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'post/post_form.html'
+    success_url = reverse_lazy('posts_all')
+    form_class = PostModelForm
+    success_message = 'Postagem salva com sucesso.'
+    # implementa o método que conclui a ação com sucesso
+    def form_valid(self, form):
+        messages.success(self.request, self.success_message)
+        return super(PostUpdateView, self).form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Editando o post'
+        return context
